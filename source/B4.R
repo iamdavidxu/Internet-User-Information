a0d93d2bcc344b2000b724e428dae6085f6a26ca
nehav22 <114948185+nehav22@users.noreply.github.com>
2022-11-30 10:29:44 -0800

    Changing chart to choropleth map

--- source/B4.R
+++ source/B4.R
@@ -1,58 +1,48 @@
 library(dplyr)
-library(ggplot2)
 library(tidyr)
+library(maps)
+library(ggplot2)
 
 
-##4 Pie charts for Internet speeds per household income levels
+#load us state map data
+us_map <- map_data("state")
 
-income_disparities <- read.csv("https://raw.githubusercontent.com/the-markup/investigation-isp/main/data/output/figs/fig3_income.csv")
-#View(income_disparities)
+#rename region col to state
+us_map <- us_map%>%
+  rename(state = region)
+
+#make base map
+ggplot() + 
+  geom_polygon(data = us_map, aes(x=long, y=lat, group=group),
+               color="black", fill = "blue")
+
+#load state disparities data (by major city only)
+state_disparities <- read.csv("https://raw.githubusercontent.com/the-markup/investigation-isp/main/data/output/tables/table1_disparities_by_city.csv")
+
+#convert column of state abb to state names
+state_disparities$state <- state.name[match(state_disparities$state, state.abb)]
+
+#convert to lowercase
+state_disparities <- state_disparities %>% mutate(state = tolower(state))
+
+#find % point difference in slow speed offers between historically D-rated and A/B-rated neighborhoods (in terms of redlining grade)
+redlining_speed <- state_disparities%>%
+  group_by(state)%>%
+  summarize(redlining_pct_pt_diff = sum(redlining_pct_pt_diff))%>%
+  select(state, redlining_pct_pt_diff)
+
+#merge data
+merged_disparities <- left_join(us_map, redlining_speed, by = "state")
 
 
-#transpose file
-transposed_income_disparities <- as.data.frame(t(income_disparities))
-
-#rename columns
-colnames(transposed_income_disparities) <- c('Low', 'Middle_lower', 'Middle_upper', 'Upper')
-
-#delete redundant row
-transposed_income_disparities <- transposed_income_disparities[!(row.names(transposed_income_disparities) %in% c("income_level", "No.service")),]
-
-#create empty column
-transposed_income_disparities$internet_speeds <- NA
-
-#create vector of internet speeds
-speeds <- c("Slow", "Medium", "Fast", "Blazing")
-
-#add vector to data frame as a new col
-transposed_income_disparities$internet_speeds <- speeds
-
-
-#create pie chart for low income
-a <- ggplot(transposed_income_disparities, aes(x ="", y = Low, fill = internet_speeds)) +
-  geom_col(color = "black") +
-  geom_text(aes(label = Low),
-      position = position_stack(vjust = 0.5))+
-  coord_polar(theta = "y") 
-
-#create pie chart for middle-lower income
-b <- ggplot(transposed_income_disparities, aes(x ="", y = Middle_lower, fill = internet_speeds)) +
-  geom_col(color = "black") +
-  geom_text(aes(label = Middle_lower),
-            position = position_stack(vjust = 0.5))+
-  coord_polar(theta = "y") 
-
-#create pie chart for middle_upper income
-c <- ggplot(transposed_income_disparities, aes(x ="", y = Middle_upper, fill = internet_speeds)) +
-  geom_col(color = "black") +
-  geom_text(aes(label = Middle_upper),
-            position = position_stack(vjust = 0.5))+
-  coord_polar(theta = "y") 
-
-#create pie chart for upper income
-d <- ggplot(transposed_income_disparities, aes(x ="", y = Upper, fill = internet_speeds)) +
-  geom_col(color = "black") +
-  geom_text(aes(label = Upper),
-            position = position_stack(vjust = 0.5))+
-  coord_polar(theta = "y") 
-
+#plot with merged
+plot_choropleth <- ggplot()
+plot_choropleth <- plot_choropleth + geom_polygon(data = merged_disparities, 
+                                                  aes(x=long, y=lat, group=group, fill = redlining_pct_pt_diff), 
+                                                  color="white", size = 0.2) +
+  theme_minimal() +
+  ggtitle("Percentage Point Difference of Lower Income Areas with Slower Internet Speeds")+
+  labs(x = "Longitude", y = "Latitude", caption = "This is a choropleth map that displays the percentage point difference of lower income areas in major cities in each state with slow internet speeds.")+
+  scale_fill_continuous(name="Percentage Point Difference of Lower Income Areas With Slow Internet", 
+                        low = "lightblue", high = "darkblue",limits = c(-0.2, 0.5), 
+                        breaks = c(-0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5), na.value = "grey")

37f57492097bee688f177097136cb5d2dde81f7b
Aidan <aidanb04@uw.edu>
2022-12-07 10:39:27 -0800

    hshad

--- InternetDisparities/data/editsagain.R
+++ InternetDisparities/data/editsagain.R
@@ -4,3 +4,5 @@ write_csv(edit, "name.csv")
 
 
 ATT_price <- read.csv("data/speed_price_att.csv.gz")
+#filter by selecting all the variables/columns that are used in B2 and B3 
+#then delete the old csvs and this file

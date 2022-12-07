fd02ecc3865130f316780b6d21d3df71530b06fe
Aidan <aidanb04@uw.edu>
2022-12-06 19:16:53 -0800

    changing files

---
+++ InternetDisparities/sourcecopy/B2.R
@@ -0,0 +1,53 @@
+
+
+## DATA
+## I pushed the cvs files on github repo, run the line and click on the file to read it :).
+
+ATT_price <- read.csv("../data/speed_price_att.csv.gz")
+
+
+
+## Packages
+library("tidyverse")
+
+## Summary
+
+Variables_ATT <- ncol(ATT_price)
+
+Observations_ATT <- nrow(ATT_price)
+
+Max_download_speed_ATT <- ATT_price %>% 
+  select(speed_down) %>% 
+  filter(speed_down == max(speed_down, na.rm = TRUE)) %>% 
+  pull(speed_down)
+
+Max_upload_speed_ATT <- ATT_price %>% 
+  select(speed_up) %>% 
+  filter(speed_up == max(speed_up , na.rm = TRUE)) %>% 
+  pull(speed_up)
+
+
+Income_ATT <- ATT_price %>% 
+  mutate(location = paste0(ATT_price$major_city , ", " , ATT_price$state)) %>% 
+  summarise(location, income_dollars_below_median)
+
+ATT_price <- ATT_price %>%
+  mutate(location = paste0(incorporated_place,", ", major_city , ", " , state))
+
+
+
+locations_with_lowest_Redling_Grade_ATT <- ATT_price %>% 
+  select(location, redlining_grade) %>% 
+  filter(redlining_grade == min(redlining_grade , na.rm = TRUE)) %>% 
+  summarise(location = unique(location))
+
+
+
+lowest_speed_in_non_white_location_ATT <- ATT_price %>% 
+  select(race_perc_non_white, speed_down, location) %>% 
+  filter(race_perc_non_white == max(race_perc_non_white , na.rm = TRUE), 
+         speed_down == min(speed_down , na.rm = TRUE)) %>% 
+  summarise(location = unique(location) , 
+            race_perc_non_white = unique(race_perc_non_white) , 
+            speed = unique(speed_down))
+
---
+++ InternetDisparities/sourcecopy/B6.R
@@ -0,0 +1,63 @@
+library(tidyverse)
+library(plotly)
+
+att <- read.csv("../data/speed_price_att.csv.gz")
+att_other <- read.csv("../data/speed_price_att_other_cities.csv.gz")
+att_total <- full_join(att, att_other)
+
+all <- att_total %>% 
+  filter(median_household_income > 0)
+
+all_WA <- filter(all, state == "WA") %>% 
+  filter(median_household_income < 250000)
+
+fiber_in_state <- att_total %>% group_by(state) %>% 
+  count(tolower(technology)) %>% 
+  na.omit()
+colnames(fiber_in_state) [2] <- "Internet_Type"
+
+#do this for each state
+fiber_in_state_WA <- fiber_in_state %>% filter(state == "WA")
+
+pie_WA <- ggplot(fiber_in_state_WA, aes(x="", y=n, fill=Internet_Type)) +
+  geom_bar(stat="identity", width=1, color="white") +
+  coord_polar("y", start=0) +
+  theme_void()
+pie_WA
+
+pie_total <- ggplot(fiber_in_state, aes(x="", y=n, fill= tolower(Internet_Type))) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  theme_void() +
+  scale_fill_discrete(name = "Internet Type", 
+                      labels = c("Copper", "Fiber", "Fiber Based")) +
+  labs(title="Types of Internet Service Purchased")
+pie_total
+
+#figure out how to add this or at least show percentages
+
+#ggplot_ly(pie_total)
+
+build_pie <- function(data){
+  pie_total <- ggplot(data, aes(x="", y=n, fill= tolower(Internet_Type))) +
+    geom_bar(stat="identity", width=1) +
+    coord_polar("y", start=0) +
+    theme_void() +
+    scale_fill_discrete(name = "Internet Type", 
+                        labels = c("Copper", "Fiber", "Fiber Based")) +
+    labs(title="Types of Internet Service Purchased Averaged Across All States")
+  return(pie_total)
+}
+
+build_state_pie <- function(data, state.choice){
+  data <- data %>% filter(state == state.choice)
+  
+  pie_state <- ggplot(data, aes(x="", y=n, fill=Internet_Type)) +
+    geom_bar(stat="identity", width=1, color="white") +
+    coord_polar("y", start=0) +
+    theme_void() +
+    scale_fill_discrete(name = "Internet Type", 
+                        labels = c("Copper", "Fiber", "Fiber Based")) +
+    labs(title="Types of Internet Service Purchased In a Certain State")
+  return(pie_state)
+}
